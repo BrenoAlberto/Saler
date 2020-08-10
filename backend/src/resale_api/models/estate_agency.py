9ee6fc22267d0a5fcc8e2a8b83fc801bd5a69da2
brenoalberto <brenoaps435@gmail.com>
2020-08-10 16:25:51 -0300

    api feature: add estate agency model

---
+++ backend/src/resale_api/models/estate_agency.py
@@ -0,0 +1,9 @@
+from resale_api import db
+
+
+class EstateAgency(db.Model):
+    __tablename__ = "estate_agency"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    address = db.Column(db.String(255), nullable=True)
