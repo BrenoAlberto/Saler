452eb09a64d6f6807c13b2ea492f64b04c716d9b
brenoalberto <brenoaps435@gmail.com>
2020-08-10 21:48:54 -0300

    api feature: update estate agency model

--- backend/src/resale_api/models/estate_agency.py
+++ backend/src/resale_api/models/estate_agency.py
@@ -5,5 +5,5 @@ class EstateAgency(db.Model):
     __tablename__ = "estate_agency"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), unique=True, nullable=False)
     address = db.Column(db.String(255), nullable=True)
